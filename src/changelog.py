60e76a1f39ed2c50e9beba257631b23936757703
ecalzo <evancalz@me.com>
2022-02-27 13:12:10 -0500

    wip: handle jobs only defined in the cloud

--- src/changelog.py
+++ src/changelog.py
@@ -7,6 +7,7 @@ from api import list_all_jobs, get_dbt_project_id
 from yaml_parser import read_jobs_yaml
 
 from deepdiff import DeepDiff
+from collections import defaultdict
 
 
 def create_changelog(cloud_jobs, configured_jobs):
@@ -17,13 +18,15 @@ def create_changelog(cloud_jobs, configured_jobs):
         job for job in all_jobs if job["project_id"] == get_dbt_project_id()
     ]
     diff = compare_jobs(current_proj_jobs, configured_jobs)
+    return diff
 
 
 def compare_jobs(current_jobs, configured_jobs):
-    job_map = dict()
+    job_map = defaultdict(dict)
     for cloud_job in current_jobs:
         job_map[cloud_job["name"]]["existing"] = cloud_job
         job_map[cloud_job["name"]]["mode"] = "update"
+
     for config_job in configured_jobs:
         if config_job["name"] not in job_map:
             # if there is no existing version of this conf, set create mode
@@ -31,8 +34,14 @@ def compare_jobs(current_jobs, configured_jobs):
             job_map[config_job["name"]]["mode"] = "create"
         job_map[config_job["name"]]["configured"] = config_job
 
-    for job_name, job_origin in job_map.items():
-        diff = DeepDiff(job_origin["existing"], job_origin["configured"])
+    cloud_only_jobs = []
+    for job_name, job_meta in job_map.items():
+        # handle jobs that are only confgured in the cloud
+        configured = job_meta.get("configured", None)
+        if configured is None:
+            cloud_only_jobs.append(job_name)
+            continue
+        diff = DeepDiff(job_meta["existing"], configured)
         print(diff)
         job_map[job_name]["diff"] = diff
 
