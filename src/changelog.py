c1c71fce84ef262ef147972b7a6f4eef1ebcf954
ecalzo <evancalz@me.com>
2022-03-02 06:50:04 -0500

    refactor: swap create_changelog_mapping into compare_jobs

--- src/changelog.py
+++ src/changelog.py
@@ -35,27 +35,15 @@ def create_changelog_mapping(current_project_jobs, configured_jobs):
 
 
 def compare_jobs(current_jobs, configured_jobs):
-    job_map = defaultdict(dict)
-    for cloud_job in current_jobs:
-        job_map[cloud_job["name"]]["existing"] = cloud_job
-        job_map[cloud_job["name"]]["mode"] = "update"
-
-    for config_job in configured_jobs:
-        if config_job["name"] not in job_map:
-            # if there is no existing version of this conf, set create mode
-            job_map[config_job["name"]]["existing"] = {}
-            job_map[config_job["name"]]["mode"] = "create"
-        job_map[config_job["name"]]["configured"] = config_job
-
+    change_map = create_changelog_mapping(current_jobs, configured_jobs)
     cloud_only_jobs = []
-    for job_name, job_meta in job_map.items():
+    for job_name, job_meta in change_map.items():
         # handle jobs that are only confgured in the cloud
         configured = job_meta.get("configured", None)
         if configured is None:
             cloud_only_jobs.append(job_name)
             continue
         diff = DeepDiff(job_meta["existing"], configured)
-        print(diff)
-        job_map[job_name]["diff"] = diff
+        change_map[job_name]["diff"] = diff
 
-    return job_map
+    return change_map
